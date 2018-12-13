f652fd1f4a3edc0d582bb37e50b0d3a330242afc
Yukun Ding <yding5@crcfe01.crc.nd.edu>
2018-12-13 11:16:51 -0500

    add standard data augmentation to cifar100 training data loader

--- dataloader.py
+++ dataloader.py
@@ -38,9 +38,10 @@ def get_data_loader(name, batch_size = 128, workers=4):
 
 
     elif name == 'cifar100':
-        transform = transforms.Compose([ transforms.ToTensor(),normalize2,])
-        train_dataset = torchvision.datasets.CIFAR100(root='../data', train=True, download=True, transform=transform)
-        val_dataset = torchvision.datasets.CIFAR100(root='../data', train=False, download=True, transform=transform)
+        train_transform = transforms.Compose([ transforms.RandomHorizontalFlip(), transforms.RandomCrop(32, 4), transforms.ToTensor(),normalize2,])
+        val_transform = transforms.Compose([ transforms.ToTensor(),normalize2,])
+        train_dataset = torchvision.datasets.CIFAR100(root='../data', train=True, download=True, transform=train_transform)
+        val_dataset = torchvision.datasets.CIFAR100(root='../data', train=False, download=True, transform=val_transform)
         train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=workers)
         val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=workers)
     else:
